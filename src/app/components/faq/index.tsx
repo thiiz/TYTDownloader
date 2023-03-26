1c5bcd80bd8ce38f88031cdf2c335d4610d34767
thiizz <afffvitu@gmail.com>
2023-03-26 10:19:20 -0300

    update

--- src/app/components/faq/index.tsx
+++ src/app/components/faq/index.tsx
@@ -6,28 +6,28 @@ export default function Faq() {
             <h1>FAQ of TYTDownloader</h1>
             <ol>
                 <li>
-                    <strong>Limita o número de ficheiros que converto?</strong><br />
-                    Não, não limitamos. É livre de converter o YouTube em MP3 quantas quiser. Mais tarde, algumas partilhas no Facebook, Twitter ou outras podem ser necessárias para que mais pessoas saibam sobre o TYTDownloader.
+                    <strong>Limita o número de arquivos que converto?</strong>
+                    <span>Não, não limitamos. É livre de converter os vídeos do YouTube em MP3 quantos quiser. Mais tarde, algumas partilhas no Facebook, Twitter ou outras podem ser necessárias para que mais pessoas saibam sobre o TYTDownloader.</span>
                 </li>
                 <li>
-                    <strong>Que formatos áudio/vídeo suporta?</strong><br />
-                    O TYTDownloader suporta a conversão para MP3 e MP4. Os áudios MP3 e os vídeos MP4 são universalmente reproduzidos em qualquer dispositivo.
+                    <strong>Que formatos áudio/vídeo suporta?</strong>
+                    <span>O TYTDownloader suporta a conversão para MP3 e MP4. Os áudios MP3 e os vídeos MP4 são universalmente reproduzidos em qualquer dispositivo.</span>
                 </li>
                 <li>
-                    <strong>Pode converter vídeos online com proteção de direitos autorais para MP3/MP4?</strong><br />
-                    Não, nós não apoiamos. O TYTDownloader suporta apenas a conversão de vídeos online para MP3/MP4 para uso pessoal.
+                    <strong>Pode converter vídeos online com proteção de direitos autorais para MP3/MP4?</strong>
+                    <span>Não, nós não apoiamos. O TYTDownloader suporta apenas a conversão de vídeos online para MP3/MP4 para uso pessoal.</span>
                 </li>
                 <li>
-                    <strong>O seu serviço está disponível em smartphone ou tablet?</strong><br />
-                    Sim, é um pouco. Os nossos serviços estão disponíveis em quaisquer tablets e telemóveis, Android, iOS e outros.
+                    <strong>O seu serviço está disponível em smartphones ou tablets?</strong>
+                    <span>Sim, está disponível. Os nossos serviços estão acessíveis em quaisquer smartphones e tablets, Android, iOS e outros.</span>
                 </li>
                 <li>
-                    <strong>Onde é guardado o ficheiro de descarregamento?</strong><br />
-                    O ficheiro descarregado é guardado na pasta de descarregamento padrão do seu navegador web.
+                    <strong>Onde é salvo o arquivo de download?</strong>
+                    <span>O arquivo baixado é salvo na pasta de download padrão do seu navegador web.</span>
                 </li>
                 <li>
-                    <strong>Quantos ficheiros posso converter ao mesmo tempo?</strong><br />
-                    Quantos quiser. Abra o TYTDownloader em vários separadores. Cole um endereço de vídeo em um separador e clique em "CONVERTE"; cole outro endereço no segundo separador e clique em "CONVERTE"; cole um terceiro...
+                    <strong>Quantos arquivos posso converter ao mesmo tempo?</strong>
+                    <span>Quantos quiser. Abra o TYTDownloader em vários guias do navegador. Cole um endereço de vídeo em um guia e clique em "CONVERTE"; cole outro endereço no segundo guia e clique em "CONVERTE"; cole um terceiro...</span>
                 </li>
             </ol>
         </FaqContainer>
